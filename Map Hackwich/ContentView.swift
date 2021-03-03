71a184747a14308d06cde72be0b71ada196b198d
Zuhair Ali <zuhairali20003@gmail.com>
2021-03-03 10:50:15 -0600

    Added Location to the map

--- Map Hackwich/ContentView.swift
+++ Map Hackwich/ContentView.swift
@@ -19,16 +19,50 @@ struct ContentView: View {
             latitudeDelta: 0.05,
             longitudeDelta: 0.05)
     )
+    @State private var places = [Place(name: "Barrington High School", coordinate: CLLocationCoordinate2D(latitude: 42.15704, longitude: -88.14812))]
+    
     var body: some View {
-       Map(coordinateRegion: $region,
-           interactionModes: .all,
+        Map(coordinateRegion: $region,
+            interactionModes: .all,
             showsUserLocation: true,
-            userTrackingMode: $userTrackingMode)
+            userTrackingMode: $userTrackingMode,
+            annotationItems: places) { place in
+            MapAnnotation(coordinate: place.coordinate,
+                          anchorPoint: CGPoint(x: 0.5, y: 1.2)) {
+                Marker(name: place.name)
+                
+            }
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+    
+    struct Place: Identifiable {
+        let id = UUID()
+        let name: String
+        let coordinate: CLLocationCoordinate2D
+    }
+    
+    struct Marker: View {
+        var name: String
+        var body: some View {
+            ZStack {
+                VStack {
+                    Spacer(minLength: 15)
+                    Rectangle()
+                        .fill(Color.black)
+                        .frame(width: 200, height: 30, alignment: .center)
+                        .rotationEffect(.degrees(45))
+                }
+                Capsule()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 30, alignment: .center)
+                Text(name)
+            }
+        }
     }
 }
